c46f5b4cff57b43db8d91dc402fe51aa9fa69aa6
tug11537 <tug11537@temple.edu>
2025-02-09 16:26:07 -0500

    changes

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const numbers = [1, 2, 3, 4, 5];
 
 let index = 0;
 
-const sum = 0;
+let sum = 0;
 
-while (index == numbers.length) {
+while (index < numbers.length) {
     sum += numbers[index];
     index = index + 1;
 }
 
 
-numbers.append(6);
+numbers.push(6);
 
 console.log(numbers);
 console.log(sum);
